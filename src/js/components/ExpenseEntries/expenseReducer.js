9b7c175b5c70bea2dab88c9ec230a7c3596c1f17
Alberto Vega <albertovega00@gmail.com>
2019-11-11 16:50:17 -0800

    add reducers

---
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -0,0 +1,10 @@
+
+const defaultState = {
+    description: '',
+    amount: '',
+    lineItems: []
+};
+
+export default function ExpenseReducer (satte = defaultState, action) {
+ return satte;   
+}
---
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -0,0 +1,10 @@
+
+const defaultState = {
+    description: '',
+    amount: '',
+    lineItems: []
+};
+
+export default function incomeReducer (state = defaultState, action) {
+    return state;
+}
